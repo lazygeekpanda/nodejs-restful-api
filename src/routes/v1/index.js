d1e83624dfa3ddc266f13d1823f68ce04c9ed9e8
Žymantas Venclauskas <30467983+lazygeekpanda@users.noreply.github.com>
2021-12-09 14:47:07 +0200

    initial

---
+++ src/routes/v1/index.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const config = require('../../config/config')
+const authRoute = require('./auth.route')
+const userRoute = require('./user.route')
+const docsRoute = require('./docs.route')
+
+const router = express.Router()
+
+const defaultRoutes = [
+  {
+    path: '/auth',
+    route: authRoute
+  },
+  {
+    path: '/user',
+    route: userRoute
+  }
+]
+
+const devRoutes = [
+  // routes available only in development mode
+  {
+    path: '/docs',
+    route: docsRoute
+  }
+]
+
+defaultRoutes.forEach((route) => router.use(route.path, route.route))
+
+if (config.env === 'development') {
+  devRoutes.forEach((route) => router.use(route.path, route.route))
+}
+
+module.exports = router
